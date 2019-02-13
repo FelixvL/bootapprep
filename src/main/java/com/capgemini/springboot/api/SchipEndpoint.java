5530dc79137808858d7d0f1ecca37d9e28b846dd
FelixvL <digitaleservice@gmail.com>
2019-02-13 13:32:04 +0100

    de jquery en endpoint werkt net

--- src/main/java/com/capgemini/springboot/api/SchipEndpoint.java
+++ src/main/java/com/capgemini/springboot/api/SchipEndpoint.java
@@ -1,14 +1,22 @@
 package com.capgemini.springboot.api;
 
+import com.capgemini.springboot.controller.SchipRepository;
+import com.capgemini.springboot.model.Schip;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SchipEndpoint {
+    @Autowired
+    private SchipRepository mijnSchipRepository;
 
     @GetMapping("hallo")
-    public String lopen(){
+    public Schip lopen(){
         System.out.println("go");
-        return "jojo";
+        Schip hetSchip = new Schip();
+        hetSchip.naam = "Titanic";
+        Schip vliegendeHollander = mijnSchipRepository.save(hetSchip);
+        return vliegendeHollander;
     }
 }
---
+++ src/main/java/com/capgemini/springboot/model/Schip.java
@@ -0,0 +1,24 @@
+package com.capgemini.springboot.model;
+
+
+import org.hibernate.validator.constraints.ISBN;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@Entity
+public class Schip {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public long id;
+
+    public String type;
+    public int lengte;
+    public int aantalPassagiers;
+    public LocalDateTime bouwdatum;
+    public String naam;
+    public int topSnelheid;
+}
