33d3cdacd98e3ad7f9c9f9e8e0beefbb04374b17
FelixvL <digitaleservice@gmail.com>
2019-02-13 16:04:11 +0100

    schepen in table

--- src/main/java/com/capgemini/springboot/api/SchipEndpoint.java
+++ src/main/java/com/capgemini/springboot/api/SchipEndpoint.java
@@ -4,6 +4,7 @@ import com.capgemini.springboot.controller.SchipRepository;
 import com.capgemini.springboot.model.Schip;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,11 +12,11 @@ public class SchipEndpoint {
     @Autowired
     private SchipRepository mijnSchipRepository;
 
-    @GetMapping("hallo")
-    public Schip lopen(){
-        System.out.println("go");
+    @GetMapping("nieuwschip/{scheepsnaam}")
+    public Schip nieuwSchipMaken(@PathVariable String scheepsnaam){
+        System.out.println("go"+scheepsnaam);
         Schip hetSchip = new Schip();
-        hetSchip.naam = "Titanic";
+        hetSchip.naam = scheepsnaam;
         Schip vliegendeHollander = mijnSchipRepository.save(hetSchip);
         return vliegendeHollander;
     }
